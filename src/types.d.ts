cb2a53736361073a573a369ce99023784315b6f6
Almazov Urmat <almazov.urmat@gmail.com>
2023-06-12 15:42:00 +0600

    Added new interface for Alert component

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,9 @@ export interface IBtnModel {
     type: string;
     label: string;
 }
+
+export interface IAlertData {
+    type: string;
+    text: string;
+    onDismiss?: (number:number) => void;
+}
